836fefdbd749734ca7ee472a64bb4d6cdcaea0a7
Tigran Avetisyan <tigran.1995.11@gmail.com>
2022-02-24 15:46:46 +0400

    Making further clarifications to code

--- api/lime-api.py
+++ api/lime-api.py
@@ -37,13 +37,12 @@ origins = ["http://localhost:3000",
 app.add_middleware(
 CORSMiddleware,
 allow_origins=origins,
-allow_credentials=True,
 allow_methods=["*"],
 allow_headers=["*"])
 
 # Endpoint for uploading model files
 @app.post("/upload-model/")
-async def load_model(weights: UploadFile, arch: UploadFile):
+async def upload_model(weights: UploadFile, arch: UploadFile):
     # Writing the received model file to server storage
     async with aiofiles.open('../outputs/received_model.h5', 'wb') as out_file:
         # Read the model weights in bytes
